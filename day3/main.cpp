1c83e29c0a87d0b26064bace2e313a8c95c23bbb
Dimitri Belopopsky <dimitri@belopopsky.com>
2017-12-03 21:14:35 +0100

    Day 3 Part 1

---
+++ day3/main.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <cmath>
+
+struct Point
+{
+
+  constexpr Point(int m_x, int m_y) : x(m_x), y(m_y)
+  {
+  }
+
+  int x = 0;
+  int y = 0;
+
+};
+
+constexpr int manhattanDistance(Point a, Point b)
+{
+  return std::abs(a.x - b.x) + std::abs(a.y - b.y);
+}
+
+constexpr Point spiral2Grid(int spiralSquare)
+{
+
+  Point pos = {0, 0};
+  int level = 1;
+  int num = 1;
+  while (num != spiralSquare)
+    {
+      num++;
+      pos.x++;
+      if (num == spiralSquare)
+	{
+	      return pos;
+	}
+      while (pos.y < level)
+	{
+	  num++;
+	  pos.y++;
+	  if (num == spiralSquare)
+	    {
+	      return pos;
+	    }
+	}
+
+      while (pos.x > -level)
+	{
+	  num++;
+	  pos.x--;
+	  if (num == spiralSquare)
+	    {
+	      return pos;
+	    }
+	}
+
+      while (pos.y > -level)
+	{
+	  num++;
+	  pos.y--;
+	  if (num == spiralSquare)
+	    {
+	      return pos;
+	    }
+	}
+
+      while (pos.x < level)
+	{
+	  num++;
+	  pos.x++;
+	  if (num == spiralSquare)
+	    {
+	      return pos;
+	    }
+	}
+
+      level++;
+    }
+  return pos;
+}
+
+int main()
+{
+
+  // Part 1
+  std::cout << "Test answer: " << manhattanDistance({0, 0}, spiral2Grid(1)) << '\n';
+  std::cout << "Test answer: " << manhattanDistance({0, 0}, spiral2Grid(12)) << '\n';
+  std::cout << "Test answer: " << manhattanDistance({0, 0}, spiral2Grid(23)) << '\n';
+  std::cout << "Test answer: " << manhattanDistance({0, 0}, spiral2Grid(1024)) << '\n';
+  std::cout << "Answer: "      << manhattanDistance({0, 0}, spiral2Grid(368078)) << '\n';
+
+
+  return 0;
+}
